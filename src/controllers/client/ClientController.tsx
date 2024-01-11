e702e6ae559676485b6854e13de107beb724a071
RT <rustytrim11@gmail.com>
2024-01-10 21:46:16 -0500

    Co-authored-by: Bryden Calaway <BrydenIsNotSmart@users.noreply.github.com>

--- src/controllers/client/ClientController.tsx
+++ src/controllers/client/ClientController.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { Client } from "@strafechat/strafe.js";
+import cookie from "js-cookie";
 import { usePathname } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from 'react';
-import cookie from "js-cookie";
 
 const ClientControllerContext = createContext<{ client: Client | null }>({
   client: null
@@ -13,23 +13,28 @@ export default function ClientController({ children }: { children: JSX.Element }
   const pathname = usePathname();
 
   const [client, setClient] = useState<Client | null>(null);
+  const [ready, setReady] = useState(false);
 
   const init = () => {
-    const client = new Client({config: {
-      equinox: "http://localhost:443/v1"
-    }});
-    client.login(`${String(cookie.get("token")!)}`).catch(() => {});
+    const client = new Client({
+      config: {
+        equinox: "http://localhost:443/v1"
+      }
+    });
+    client.login(`${String(cookie.get("token")!)}`).catch(() => { });
     setClient(client);
   }
 
   useEffect(() => {
-    if(!cookie.get("token")! && !["/login", "/register", "/verify"].includes(pathname)) window.location.href = "/login";
+    if (!cookie.get("token")! && !["/login", "/register", "/verify"].includes(pathname)) window.location.href = "/login";
     else if (!client && !["/login", "/register", "/verify"].includes(pathname)) init();
   }, [client, pathname]);
 
+  console.log(client);
+
   return (
     <ClientControllerContext.Provider value={{ client }}>
-      {children}
+      {ready ? children : <></>}
     </ClientControllerContext.Provider>
   )
 }
