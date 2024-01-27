afb05520f26507fa9263eb3df74faf9e0a20f049
RT <rustytrim11@gmail.com>
2024-01-27 16:59:41 -0500

    feat: update settings

--- src/components/settings/user/AccountSettings.tsx
+++ src/components/settings/user/AccountSettings.tsx
@@ -1,34 +1,42 @@
 import { Button } from "@/components/ui/button";
-import { useClient, useModal } from "@/hooks";
+import { formatDiscrim } from "@/helpers/formatter";
+import { useClient, useForceUpdate, useModal } from "@/hooks";
 import { useState } from "react";
 
 export default function AccountSettings() {
 
     const { client } = useClient();
     const { openModal } = useModal();
+    const forceUpdate = useForceUpdate();
 
     const [savedData, setSavedData] = useState({
         username: client?.user?.username!,
         email: client?.user?.email!,
-        phone_number: client!.user!.phone_number
+        phone_number: client?.user?.phone_number,
+        discriminator: client?.user?.discriminator!,
+        locale: client?.user?.locale!,
     });
 
-    const [data, setData] = useState({
-        username: client?.user?.username!,
-        email: client?.user?.email!,
-        phone_number: client!.user!.phone_number
-    });
+    const [data, setData] = useState(savedData);
 
     const saveData = () => {
-        setSavedData(data);
+        console.log(data);
+        client?.user?.edit(data).catch((err) => console.error(err)).then(() => setSavedData(data));
+        forceUpdate();
     }
 
     return (
         <>
-            {JSON.stringify(data) != JSON.stringify(savedData) && <Button className="save" onClick={saveData}>Save</Button>}
+            {JSON.stringify(data) != JSON.stringify(savedData) &&
+                <div className="action">
+                    <Button className="bg-destructive" onClick={() => setData(savedData)}>Reset</Button>
+                    <Button onClick={saveData}>Save</Button>
+                </div>
+            }
             <h1 className="title">My Account</h1>
             <div className="account">
                 <div className="card">
+
                     <div className="item">
                         <div className="first">
                             <label>Username</label>
@@ -36,12 +44,32 @@ export default function AccountSettings() {
                         </div>
                         <div className="final">
                             <Button onClick={() => openModal("edit-data", {
-                                title: "Edit Username", defaultValue: client?.user?.username, type: "username", set: (username: string) => {
-                                    setData({ ...data, username });
-                                }
+                                title: "Edit Username", inputs: [{
+                                    defaultValue: data.username, placeholder: "Username", label: "Username", set: (username: string) => {
+                                        setData({ ...data, username: username || data.username });
+                                    }
+                                }]
                             })}>Edit</Button>
                         </div>
                     </div>
+
+                    <div className="item">
+                        <div className="first">
+                            <label>Discriminator</label>
+                            <span>{formatDiscrim(data.discriminator)}</span>
+                        </div>
+                        <div className="final">
+                            <Button onClick={() => openModal("edit-data", {
+                                title: "Edit Discriminator", inputs: [{
+                                    defaultValue: data.discriminator, placeholder: "Discriminator", label: "Discriminator", set: (discriminator: string) => {
+                                        if (isNaN(parseInt(discriminator))) return;
+                                        setData({ ...data, discriminator: parseInt(discriminator) || data.discriminator });
+                                    }
+                                }]
+                            })}>Edit</Button>
+                        </div>
+                    </div>
+
                     <div className="item">
                         <div className="first">
                             <label>Email</label>
@@ -52,12 +80,34 @@ export default function AccountSettings() {
                         </div>
                         <div className="final">
                             <Button onClick={() => openModal("edit-data", {
-                                title: "Edit Email", defaultValue: client?.user?.email, type: "email", set: (email: string) => {
-                                    setData({ ...data, email });
-                                }
+                                title: "Edit Email", inputs: [{
+                                    defaultValue: data.email, placeholder: "Email", label: "Email", set: (email: string) => {
+                                        setData({ ...data, email: email || data.email });
+                                    }
+                                }]
                             })}>Edit</Button>
                         </div>
                     </div>
+
+                    <div className="item">
+                        <div className="first">
+                            <label>Password</label>
+                            <span>********</span>
+                        </div>
+                        <div className="final">
+                            <Button className="disabled" onClick={() => {
+                                return;
+                                openModal("edit-data", {
+                                    title: "Edit Password", inputs: [{
+                                        placeholder: "Current Password", label: "Current Password", set: (password: string) => console.log("Current Password", password)
+                                    }, {
+                                        placeholder: "New Password", label: "New Password", set: (password: string) => console.log("New Password", password)
+                                    }]
+                                })
+                            }}>Edit</Button>
+                        </div>
+                    </div>
+
                     <div className="item">
                         <div className="first">
                             <label>Phone Number</label>
@@ -68,14 +118,47 @@ export default function AccountSettings() {
                         <div className="final">
                             {data.phone_number ? (
                                 <>
-                                    <Button className="bg-destructive">Remove</Button>
-                                    <Button onClick={() => openModal("edit-data", {
-                                        title: "Edit Phone Number", type: "phone_number", set: (phone_number: string) => {
-                                            setData({ ...data, phone_number });
-                                        }
+                                    <Button className="bg-destructive disabled">Remove</Button>
+                                    <Button className="disabled" onClick={() => openModal("edit-data", {
+                                        title: "Edit Phone Number", inputs: [{
+                                            defaultValue: data.phone_number, placeholder: "Phone Number", label: "Phone Number", set: (phone_number: string) => {
+                                                setData({ ...data, phone_number: phone_number || data.phone_number });
+                                            }
+                                        }]
                                     })}>Edit</Button>
                                 </>
-                            ) : <Button>Add</Button>}
+                            ) : <Button className="disabled">Add</Button>}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="item">
+                        <div className="first">
+                            <label>2FA</label>
+                            <span>{false ? "Enabled" : "Disabled"}</span>
+                        </div>
+                        <div className="final">
+                            <Button className="disabled">Edit</Button>
+                        </div>
+                    </div>
+
+                    {/* <div className="item">
+                        <div className="first">
+                            <label>Locale</label>
+                            <span>{data.locale.toUpperCase()}</span>
+                        </div>
+                        <div className="final">
+                            <Button className="disabled">Edit</Button>
+                        </div>
+                    </div> */}
+
+                    <div className="item">
+                        <div className="first">
+                            <Button className="bg-destructive hover:bg-destructive disabled" onClick={() => {
+                                return;
+                                openModal("delete-account")
+                            }}>Delete Account</Button>
                         </div>
                     </div>
                 </div>
--- src/controllers/modals/components/SettingsModal.tsx
+++ src/controllers/modals/components/SettingsModal.tsx
@@ -1,16 +1,18 @@
 import SettingsNotFound from "@/components/settings/SettingsNotFound";
 import DesktopSettings from "@/components/settings/app/DesktopSettings";
+import AccountSettings from "@/components/settings/user/AccountSettings";
 import NotificationSettings from "@/components/settings/user/NotificationSettings";
 import { AnimatePresence, motion } from "framer-motion";
+import cookie from "js-cookie";
 import {
   FaBell,
   FaDesktop,
   FaIdCard,
+  FaRightFromBracket,
   FaUser,
   FaUserLock
 } from 'react-icons/fa6';
 import Modal from './Modal';
-import AccountSettings from "@/components/settings/user/AccountSettings";
 
 const modalVariants = {
   open: { opacity: 1, transition: { ease: "backIn", duration: 0.3, x: { duration: 1 } } },
@@ -24,6 +26,13 @@ class SettingsModal extends Modal<{}, {}> {
   }
 
   render() {
+
+    const logout = () => {
+      cookie.remove("token");
+      this.close();
+      window.location.href = "/login";
+    }
+
     return (
       <>
         <AnimatePresence>
@@ -38,13 +47,15 @@ class SettingsModal extends Modal<{}, {}> {
               <div className="sidebar">
                 <ul>
                   <li className="title">User Settings</li>
-                  <li onClick={() => this.setState({ currentTab: "account" })}><FaUser /> &nbsp;&nbsp;My Account</li>
-                  <li onClick={() => this.setState({ currentTab: "profile" })}><FaIdCard />&nbsp;&nbsp;Profile</li>
-                  <li onClick={() => this.setState({ currentTab: "authorized-apps" })}><FaUserLock />&nbsp;&nbsp;Authorized Apps</li>
+                  <li className={this.state.currentTab === "account" ? "active" : ""} onClick={() => this.setState({ currentTab: "account" })}><FaUser /> &nbsp;&nbsp;My Account</li>
+                  <li className={this.state.currentTab === "profile" ? "active" : ""} onClick={() => this.setState({ currentTab: "profile" })}><FaIdCard />&nbsp;&nbsp;Profile</li>
+                  <li className={this.state.currentTab === "authorized-apps" ? "active" : ""} onClick={() => this.setState({ currentTab: "authorized-apps" })}><FaUserLock />&nbsp;&nbsp;Authorized Apps</li>
                   <hr />
                   <li className="title">App Settings</li>
-                  <li onClick={() => this.setState({ currentTab: "notifications" })}><FaBell />&nbsp;&nbsp;Notifications</li>
-                  <li onClick={() => this.setState({ currentTab: "desktop" })}><FaDesktop />&nbsp;&nbsp;Desktop</li>
+                  <li className={this.state.currentTab === "notifications" ? "active" : ""} onClick={() => this.setState({ currentTab: "notifications" })}><FaBell />&nbsp;&nbsp;Notifications</li>
+                  <li className={this.state.currentTab === "desktop" ? "active" : ""} onClick={() => this.setState({ currentTab: "desktop" })}><FaDesktop />&nbsp;&nbsp;Desktop</li>
+                  <hr />
+                  <li onClick={() => logout()} className="!text-destructive"><FaRightFromBracket />&nbsp;&nbsp; Logout</li>
                 </ul>
               </div>
               <button className="close" onClick={() => this.close()}>X</button>
