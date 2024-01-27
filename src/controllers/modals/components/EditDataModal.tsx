7fb2960cefd4257fb25e82acc2d531a5c0f90059
RT <rustytrim11@gmail.com>
2024-01-27 16:58:15 -0500

    feat: improve edit-data-modal

--- src/controllers/modals/components/EditDataModal.tsx
+++ src/controllers/modals/components/EditDataModal.tsx
@@ -7,21 +7,24 @@ const modalVariants = {
     closed: { opacity: 0, transition: { ease: "backOut", duration: 0.3, x: { duration: 1 } } },
 };
 
-class EditDataModel extends Modal<{ data: string }, { data: { title: string, defaultValue: string, type: string, set: Dispatch<SetStateAction<any>> } }> {
+class EditDataModel extends Modal<{ data: any[] }, { data: { title: string, inputs: (React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & { set: Dispatch<SetStateAction<any>> })[] } }> {
 
     constructor(props: any) {
         super(props);
     }
 
     state = {
-        data: ""
+        data: []
     }
 
     render() {
-
         const handleSubmit = (event: FormEvent) => {
             event.preventDefault();
-            this.props.data.set(this.state.data);
+
+            for (const input of this.props.data.inputs) {
+                input.set(this.state.data[this.props.data.inputs.indexOf(input)]);
+            }
+
             this.close();
         }
 
@@ -39,7 +42,16 @@ class EditDataModel extends Modal<{ data: string }, { data: { title: string, def
                             <div className="modal-window" style={{ width: "300px" }}>
                                 <form onSubmit={handleSubmit}>
                                     <h1>{this.props.data.title}</h1>
-                                    <input defaultValue={this.props.data.defaultValue} onChange={(event) => this.setState({ data: event.target.value })} />
+                                    <div className="flex flex-col gap-2 py-2">
+                                        {this.props.data.inputs.map((input, index) => {
+                                            return (
+                                                <div key={index}>
+                                                    <label>{input.placeholder}</label>
+                                                    <input {...input} onChange={(event) => this.setState({ data: [...this.state.data.slice(0, index), event.target.value, ...this.state.data.slice(index + 1)] })} />
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
                                     <div className="flex justify-end gap-4 mt-2">
                                         <button type="button" onClick={() => this.close()}>Cancel</button>
                                         <button type="submit">Save</button>
