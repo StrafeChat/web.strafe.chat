a84fadcb65e746eb57c7803df1d064f0a5f10a13
RT <rustytrim11@gmail.com>
2024-01-28 02:04:44 -0500

    feat: spaces indicator

--- src/components/spaces/SpaceList.tsx
+++ src/components/spaces/SpaceList.tsx
@@ -2,6 +2,7 @@ import { useClient, useModal } from "@/hooks";
 import Link from "next/link";
 import { FaCompass, FaGear, FaPenToSquare, FaPlus } from "react-icons/fa6";
 import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "../ui/context-menu";
+import { NavLink } from "../shared";
 
 export default function SpaceList() {
 
@@ -34,18 +35,22 @@ export default function SpaceList() {
          </ContextMenu>
          <div className="seperator" />
          <div className="spaces">
-            <Link href="/spaces/strafe">
-               <button className="space">
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img className="space" src="https://cdn.discordapp.com/icons/1125943618876735500/03625cebd9f4304c834c07625f2a4f1e.webp?size=4096" alt=""></img>
-               </button>
-            </Link>
-            <Link href="/spaces/trumpfanclub">
-               <button className="space">
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img className="space" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.kjzz.org%2Fs3fs-public%2Fdonald-trump-170818.jpg&f=1&nofb=1&ipt=0fa3dab62d6f1302ce1b234302429284a4e5b0128a3bcc8913daf08bfbd7465f&ipo=images" alt=""></img>
-               </button>
-            </Link>
+            <div>
+               <NavLink href="/spaces/strafe">
+                  <button className="space">
+                     {/* eslint-disable-next-line @next/next/no-img-element */}
+                     <img className="space" src="https://cdn.discordapp.com/icons/1125943618876735500/03625cebd9f4304c834c07625f2a4f1e.webp?size=4096" alt=""></img>
+                  </button>
+               </NavLink>
+            </div>
+            <div>
+               <NavLink href="/spaces/trumpfanclub">
+                  <button className="space">
+                     {/* eslint-disable-next-line @next/next/no-img-element */}
+                     <img className="space" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.kjzz.org%2Fs3fs-public%2Fdonald-trump-170818.jpg&f=1&nofb=1&ipt=0fa3dab62d6f1302ce1b234302429284a4e5b0128a3bcc8913daf08bfbd7465f&ipo=images" alt=""></img>
+                  </button>
+               </NavLink>
+            </div>
          </div>
 
          <div className="seperator" />
