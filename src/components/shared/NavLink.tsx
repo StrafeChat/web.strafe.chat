c61fba742699419a91dcb790405b7ebab4d1e751
RT <rustytrim11@gmail.com>
2024-01-28 02:06:46 -0500

    fix: NavLink having some issues on the / route

--- src/components/shared/NavLink.tsx
+++ src/components/shared/NavLink.tsx
@@ -6,7 +6,11 @@ export function NavLink(props: NavLinkProps) {
 
     const pathname = usePathname();
 
+    console.log(pathname.slice(1), props.href.toString().slice(1));
+
+    if (props.href.toString().trim() === '/' && pathname.toString().trim() !== '/') return <Link {...props}>{props.children}</Link>
+
     return (
-        <Link {...props} {...{ "active": `${pathname == props.href}` }}>{props.children}</Link>
+        <Link {...props} {...{ "active": `${pathname.slice(1).includes(props.href.toString().slice(1))}` }}>{props.children}</Link>
     )
 }
