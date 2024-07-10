27810396e33af92bdbfb70b150280fe73a234965
ShadowLp174 <samuel.moravec@yahoo.com>
2024-07-10 21:54:19 +0200

    commit pre merge

---
+++ src/components/chat/voice/VoiceRoomPreview.tsx
@@ -0,0 +1,12 @@
+export function VoiceRoomPreview(props: {}) {
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      gap: "1rem",
+    }}>
+      
+    </div>
+  )
+}
