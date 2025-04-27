5904ae3a1beda822e1e186821f1278ed5074eb61
bhuci <asd@wp>
2025-04-27 18:39:41 +0200

    Added AnimatedCheckbox, and updated CreatePMModal

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,6 +52,23 @@ module.exports = {
           "0%,70%,100%": { opacity: "1" },
           "20%,50%": { opacity: "0" },
         },
+        "draw-checkmark": {
+        "0%": {
+          strokeDashoffset: "24", 
+          transform: "scale(1.1)", 
+          opacity: 0
+        },
+        "50%": {
+          strokeDashoffset: "12", 
+          transform: "scale(1.05)", 
+          opacity: 1
+        },
+        "100%": {
+          strokeDashoffset: "0", 
+          transform: "scale(1)", 
+          opacity: 1
+        },
+        },
         fadein: {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
@@ -93,6 +110,7 @@ module.exports = {
         "collapsible-down": "collapsible-down 0.2s ease-out",
         "collapsible-up": "collapsible-up 0.2s ease-out",
         "caret-blink": "caret-blink 1.25s ease-out infinite",
+        "draw-checkmark": "draw-checkmark 0.8s ease-out forwards",
         fadein: "fadein 200ms ease-out forwards",
         fadeout: "fadeout 200ms ease-in forwards",
         slidein: "slidein 200ms ease-out forwards",
